0d9d0b7f86e4e62f7ed33db0aecbd270e8907d81
Ryan Salcido <rsalcido@goraft.tech>
2023-02-20 16:31:50 -0800

    Initial commit

---
+++ deploy.sh
@@ -0,0 +1,47 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+kubectl cluster-info
+
+export SOPS_AGE_KEY_FILE=~/flux-example-key.txt
+
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd "$SCRIPT_DIR"
+
+function usage() {
+  cat <<EOF
+$(basename "$0") -e <ENVIRONMENT NAME>
+
+ -e
+   environment/overlay to deploy
+EOF
+}
+
+while getopts ":e:" opt; do
+  case $opt in
+    e)
+      ENVIRONMENT_NAME="$OPTARG"
+      ;;
+    \?)
+      usage
+      exit 1
+      ;;
+  esac
+done
+
+if [[ -z "${ENVIRONMENT_NAME-}" ]]; then
+  usage
+  exit 1
+fi
+
+# Bootstrap the environment
+sops -d bootstrap.enc.yaml | kubectl apply -f-
+
+# Install Flux
+echo >&2 "Installing Flux ..."
+kubectl apply -k flux/envs/${ENVIRONMENT_NAME}
+
+# Deploy the environment
+echo >&2 "Deploying environment ${ENVIRONMENT_NAME} ..."
+kubectl apply -k "k8s/kustomizations/envs/${ENVIRONMENT_NAME}"
